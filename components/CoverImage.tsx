19c1ad784354769ac4b533e63efd925c9efe809c
Niatse <gameerseba@gmail.com>
2023-06-28 12:16:51 -0300

    v1

---
+++ components/CoverImage.tsx
@@ -0,0 +1,47 @@
+import cn from 'classnames'
+import { urlForImage } from 'lib/sanity.image'
+import Image from 'next/image'
+import Link from 'next/link'
+
+interface CoverImageProps {
+  title: string
+  slug?: string
+  image: any
+  priority?: boolean
+}
+
+export default function CoverImage(props: CoverImageProps) {
+  const { title, slug, image: source, priority } = props
+  const image = source?.asset?._ref ? (
+    <div
+     
+    >
+        <div className="w-full overflow-hidden relative" style={{ height: '40vh' , width: 'auto' }}>
+         <Image
+      layout="fill" 
+      objectFit="cover"
+    
+        alt={`Cover Image for ${title}`}
+        src={urlForImage(source).height(1000).width(2000).url()}
+        sizes="100vw"
+        priority={priority}
+      />
+    </div>
+    
+    </div>
+  ) : (
+    <div style={{ paddingTop: '50%', backgroundColor: '#ddd' }} />
+  )
+
+  return (
+    <div className="sm:mx-0">
+      {slug ? (
+        <Link href={`/posts/${slug}`} aria-label={title}>
+          {image}
+        </Link>
+      ) : (
+        image
+      )}
+    </div>
+  )
+}
---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Head, Html, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body className="bg-slate-950	 text-slate-200">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
